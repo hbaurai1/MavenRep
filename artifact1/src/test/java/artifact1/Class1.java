5a0433ad4ce987e370633eb4f3292f7965c5d82c
harsh <baurai.harsh@gmail.com>
2017-08-19 19:05:06 +0530

    First commit

---
+++ artifact1/src/test/java/artifact1/Class1.java
@@ -0,0 +1,70 @@
+package artifact1;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Class1 {
+	
+	@BeforeSuite
+	  public void beforeSuite() {
+	    System.out.println("BeforeSuite");
+	  }
+
+	  @BeforeTest
+	  public void beforeTest() {
+	    System.out.println("BeforeTest");
+	  }
+
+	  @BeforeClass
+	  public void beforeClass() {
+	    System.out.println("BeforeClass");
+	  }
+
+	  @BeforeMethod
+	  public void beforeMethod() {
+	    System.out.println("BeforeMethod");
+	  }
+
+	  @AfterMethod
+	  public void afterMethod() {
+	    System.out.println("AfterMethod");
+	  }
+
+	  @AfterClass
+	  public void afterClass() {
+	    System.out.println("AfterClass");
+	  }
+
+	  @AfterTest
+	  public void afterTest() {
+	    System.out.println("AfterTest");
+	  }
+
+	  @AfterSuite
+	  public void afterSuite() {
+	    System.out.println("AfterSuite");
+	  }
+		
+		
+		
+	  @Test
+	  public void myTestMethod3() {
+	    System.out.println("myTestMethod3");
+	  }
+
+	  @Test
+	  public void myTestMethod4() {
+	    System.out.println("myTestMethod4");
+	  }
+		
+		
+
+
+}
---
+++ artifact1/src/test/java/artifact1/Class2.java
@@ -0,0 +1,45 @@
+package artifact1;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Class2 {
+	
+	@Test
+	public void Login1() throws InterruptedException
+	// TODO Auto-generated method stub
+	{
+	/*System.setProperty("webdriver.gecko.driver", "C:\\Program Files\\Java\\geckodriver.exe");
+	FirefoxDriver driver = new FirefoxDriver();
+	Thread.sleep(5);
+	driver.get("http://apps.qaplanet.in/qahrm/login.php");
+	Thread.sleep(5);
+	driver.close();
+	driver.quit();*/
+		
+		System.out.println("----- login method");
+	
+	}
+	
+	@Test
+	public void sum1()
+	
+	{
+		int a = 10;
+		int b = 20;
+		int c= a+b;
+		System.out.println("Sum is "+c );
+	}
+	
+@Test
+public void mul1()
+	
+	{
+		int d = 10;
+		int e = 20;
+		int f= d*e;
+		System.out.println("Sum is "+f );
+	}
+
+
+}
